e7308178e6788944495167c9b5b37150c424574d
Dev-Loigen <loigen.lariosa@student.passerellesnumeriques.org>
2024-12-08 22:43:05 -0800

    update

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    username: { type: String, required: true, unique: true }, // Added username
-    idNumber: { type: String, required: true, unique: true }, // Added idNumber
+    username: { type: String, required: false, unique: true }, // Added username
+    idNumber: { type: String, required: false, unique: true }, // Added idNumber
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: {
